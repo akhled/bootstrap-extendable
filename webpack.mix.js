7d18a16f75f787aa1c891f464bdc066b895109db
akhaled <hi@amrography.com>
2020-11-10 23:12:28 +0200

    run first build!

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,14 @@
-const mix = require('laravel-mix');
+let mix = require('laravel-mix');
 
-mix.sass('scss/bootstrap-extendable.scss', 'dist/bootstrap-extend.css');
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+mix.sass('scss/bootstrap-extendable.scss', 'dist/bootstrap-extendable.css').setPublicPath('dist');
